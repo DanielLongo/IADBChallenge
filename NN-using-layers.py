1f2343c79df347c782e06036bef13fac622a70fe
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-07-22 20:57:41 -0700

    eval added

---
+++ NN-using-layers.py
@@ -0,0 +1,138 @@
+import tensorflow as tf
+from ReadData import read_data
+from tensorflow.python.framework import ops
+import matplotlib.pyplot as plt
+
+class NN(object):
+	def __init__(self):
+		self.epochs = None
+		self.lr = None
+		self.costs = []
+		self.data_filepath = "./data/train.csv"
+		self.runs = []
+		#each run is a dict {costs:list, train_acc: float, eval_acc:float, lr:float, epochs:int}
+
+	def create_placeholders(self, ):
+		self.placeholder_x = tf.placeholder(tf.float32, shape=(None, 140))
+		self.placeholder_y = tf.placeholder(tf.float32, shape=(None, 4))
+
+	def foward_pass(self, x):
+		a1 = tf.contrib.layers.fully_connected(x, 100)
+		a2 = tf.contrib.layers.fully_connected(a1, 64)
+		a3 = tf.contrib.layers.fully_connected(a2, 16)
+		a4 = tf.contrib.layers.fully_connected(a3, 4, activation_fn=tf.nn.sigmoid)
+		return a4
+
+
+	def loss_op(self, preds, y):
+		costs = tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=y)
+		avg_cost = tf.reduce_mean(costs)
+		return avg_cost
+
+	def build_NN(self):
+		ops.reset_default_graph()
+		self.create_placeholders()
+		self.preds = self.foward_pass(self.placeholder_x)
+		self.avg_cost = self.loss_op(self.preds, self.placeholder_y)
+		self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.avg_cost)	
+		self.initializer = tf.global_variables_initializer()
+
+	def load_data(self):
+		self.train_x, self.train_y, self.eval_x, self.eval_y, self.test_x, self.test_y = read_data(self.data_filepath)
+
+	def train_NN(self, epochs, lr, show_graph=True):
+		self.epochs = epochs
+		self.lr = lr
+		nn.build_NN()
+		# tf.reset_default_graph()
+		self.costs = []
+		batch_costs = []
+		with tf.Session() as sess:
+			sess.run(self.initializer)
+			for i in range(1, self.epochs + 1):
+				for b in range(len(self.train_x)):
+					cur_batch_x = self.train_x[b]
+					cur_batch_y = self.train_y[b]
+					cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
+					_, cur_cost, preds = sess.run([self.optimizer, self.avg_cost, self.preds], feed_dict=cur_feed)
+
+
+					if (i % 10 == 0) and (b == 1):
+						print("Cost @", i, cur_cost)
+					batch_costs += [cur_cost]
+				self.costs += [sum(batch_costs)/len(batch_costs)]
+
+			if show_graph:
+				y = self.costs
+				x = [i for i in range(len(y))]
+
+				plt.plot(x, y)
+				plt.ylabel("Cost")
+				plt.xlabel("Epoch")
+				plt.show()
+
+			score = self.eval_NN()
+			train_acc = score["avg_train_acc"]
+			eval_acc = score["avg_eval_acc"]
+			run_log = {"costs": self.costs, "train_acc": train_acc, "eval_acc": eval_acc, "lr" : self.lr, "epochs" : self.epochs}
+			self.runs += [run_log]
+
+			return sess
+
+	def eval_NN(self):
+		preds = tf.argmax(self.preds, 1)
+		labels = tf.argmax(self.placeholder_y, 1)
+		correct = tf.equal(preds, labels)
+		accuracy = tf.reduce_mean(tf.cast(correct, "float"))
+		
+		train_acc = []
+		for b in range(len(self.train_x)):
+			cur_batch_x = self.train_x[b]
+			cur_batch_y = self.train_y[b]
+			cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
+			cur_train_acc = accuracy.eval(cur_feed)
+			train_acc += [cur_train_acc]
+		avg_train_acc = sum(train_acc)/len(train_acc)
+
+		eval_acc = []
+		for b in range(len(self.eval_x)):
+			cur_batch_x = self.eval_x[b]
+			cur_batch_y = self.eval_y[b]
+			cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
+			cur_eval_acc = accuracy.eval(cur_feed)
+			eval_acc += [cur_eval_acc]
+		avg_eval_acc = sum(eval_acc)/len(eval_acc)
+
+		print("Average train accuracy:", avg_train_acc*100 , "%")
+		print("Average eval accuracy:", avg_eval_acc*100 , "%")
+
+		return {"avg_train_acc" : avg_train_acc, "avg_eval_acc" : avg_eval_acc}
+
+	def compare_runs(self):
+		for run in self.runs:
+			costs = run["costs"]
+			train_acc = run["train_acc"]
+			eval_acc = run["eval_acc"]
+			lr = run["lr"]
+			epochs = run["epochs"]
+			label = "epochs: " + str(epochs) + ", lr: " + str(lr)
+			self.plot_run(costs, label)
+		plt.xlabel('Epoch')
+		plt.ylabel('Loss')
+		# plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+		plt.show()
+
+
+	def plot_run(self, costs, label):
+		epochs = [i for i in range(len(costs))]
+		plt.plot(x=epochs, y=costs, label=label)
+
+
+
+
+nn = NN()
+nn.load_data()
+nn.train_NN(20, .000005, show_graph=False)
+nn.train_NN(15, .000005, show_graph=False)
+nn.train_NN(10, .000005, show_graph=False)
+nn.compare_runs()
---
+++ NN.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+from ReadData import read_data
+import numpy as np
+import pandas as pd
+from tensorflow.python.framework import ops
+import matplotlib.pyplot as plt
+
+def create_placeholders():
+	placeholder_x = tf.placeholder(tf.float32, shape=(None, 140))
+	placeholder_y = tf.placeholder(tf.float32, shape=(None, 4))
+	return placeholder_x, placeholder_y
+
+def create_variables():
+	w1 = tf.get_variable("W1", shape=[140, 100], initializer=tf.zeros_initializer())
+	b1 = tf.get_variable("B1", shape=[100], initializer=tf.zeros_initializer())
+	w2 = tf.get_variable("W2", shape=[100, 64], initializer=tf.zeros_initializer())
+	b2 = tf.get_variable("B2", shape=[64], initializer=tf.zeros_initializer())
+	w3 = tf.get_variable("W3", shape=[64, 16], initializer=tf.zeros_initializer())
+	b3 = tf.get_variable("B3", shape=[16], initializer=tf.zeros_initializer())
+	w4 = tf.get_variable("W4", shape=[16, 4], initializer=tf.zeros_initializer())
+	b4 = tf.get_variable("B4", shape=[4], initializer=tf.zeros_initializer())
+
+	params = [[w1,b1], [w2,b2], [w3,b3], [w4,b4]]
+	return params
+
+def foward_pass(x, params):
+	w1, b1 = params[0][0], params[0][1]
+	w2, b2 = params[1][0], params[1][1]
+	w3, b3 = params[2][0], params[2][1]
+	w4, b4 = params[3][0], params[3][1]
+
+	z1 = tf.matmul(x, w1) + b1
+	a1 = tf.nn.relu(z1)
+	
+	z2 = tf.matmul(a1, w2) + b2
+	a2 = tf.nn.relu(z2)
+
+	z3 = tf.matmul(a2, w3) + b3
+	a3 = tf.nn.relu(z3)
+
+	z4 = tf.matmul(a3, w4) + b4
+	a4 = tf.nn.sigmoid(z4)
+
+	return a4
+
+def loss_op(preds, y):
+	costs = tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=y)
+	avg_cost = tf.reduce_mean(costs)
+	return avg_cost
+
+def train_NN(train_x, train_y, epoch, lr=.01, show_graph=True):
+	tf.reset_default_graph()
+	ops.reset_default_graph()
+	placeholder_x, placeholder_y = create_placeholders()
+	params = create_variables()
+	preds = foward_pass(placeholder_x, params)
+	avg_cost = loss_op(preds, placeholder_y)
+	optimizer = tf.train.AdamOptimizer(lr).minimize(avg_cost)
+	
+	initialize = tf.global_variables_initializer()
+
+	costs = []
+	batch_costs = []
+	with tf.Session() as sess:
+		sess.run(initialize)
+		for i in range(1, epoch + 1):
+			for b in range(len(train_x)):
+				_, cur_cost, cur_preds, cur_params = sess.run([optimizer, avg_cost, preds, params], {placeholder_x : train_x[b], placeholder_y : train_y[b]})
+				# print("Current cost", cur_cost)
+				if (i % 10 == 0) and (b == 1):
+					print("Cost @", i, cur_cost)
+				batch_costs += [cur_cost]
+			costs += [sum(batch_costs)/len(batch_costs)]
+
+		if show_graph:
+			x = [i for i in range(len(costs))]
+			y = costs
+
+			plt.plot(x, y)
+			plt.ylabel("Cost")
+			plt.xlabel("Epoch")
+			plt.show()
+
+		return sess
+
+def test_NN(test_x, test_y, sess):
+	
+	with tf.Session as sess:
+		preds = tf.argmax
+
+train_x, train_y, eval_x, eval_y, test_x, test_y = read_data("./data/train.csv")
+
+# print("train_x", pd.isnull(np.asarray(train_x)).any())
+# print("train_y", pd.isnull(np.asarray(train_y)).any())
+# print("eval_x", pd.isnull(np.asarray(eval_x)).any())
+# print("eval_y", pd.isnull(np.asarray(eval_y)).any())
+# print("test_x", pd.isnull(np.asarray(test_x)).any())
+# print("test_y", pd.isnull(np.asarray(test_y)).any())
+sess = train_NN(train_x, train_y, 100, lr=.001)
+
+
+
+
+
