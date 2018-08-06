bc385032028f1e3e002e923243e87bac0690b6f6
Daniel Longo <DanielLongo@daniels-mbp-3.lan>
2018-08-05 21:47:00 -0700

    eval not working

--- NN-using-layers.py
+++ NN-using-layers.py
@@ -9,39 +9,14 @@ import os
 from random import shuffle
 class NN(object):
 	def __init__(self):
-		self.epochs = None
-		self.lr = None
-		self.costs = []
 		self.data_filepath = "./data/train.csv"
 		self.logs_path = "./tensorboard"
-		self.iter_counter = 0 #counts iterations of model for summary writer
 		self.runs = []
 		#each run is a dict {costs:list, train_acc: float, eval_acc:float, lr:float, epochs:int}
 
 	def create_placeholders(self, ):
 		self.placeholder_x = tf.placeholder(tf.float32, shape=(None, 140))
 		self.placeholder_y = tf.placeholder(tf.float32, shape=(None, 4))
-
-
-	def log_layer(self, layer_name, w=True, b=True, a=False, x=False):
-		#TODO: get w, a and x to work
-
-		layer_number = layer_name[-1]
-		with tf.variable_scope(layer_name, reuse=True):
-			if w:
-				weights = tf.get_variable("kernel")
-				summary_name = "w" + layer_number
-				tf.summary.histogram(summary_name, w)
-			if b:
-				bias  = tf.get_variable("bias")
-				summary_name = "b" + layer_number
-				tf.summary.histogram(summary_name, bias)
-			if a != False:
-				summary_name = "a" + layer_number
-				tf.summary.histogram(a, summary_name)
-			if x != False:
-				summary_name = "x" + layer_number
-				tf.summary.histogram(x, summary_name)
 
 	def foward_pass(self, x):
 		# a1 = tf.contrib.layers.fully_connected(x, 100)
@@ -90,36 +65,38 @@ class NN(object):
 		avg_cost = tf.reduce_mean(costs)
 		return avg_cost
 
-	def build_NN(self):
-		ops.reset_default_graph()
-		tf.reset_default_graph()
+	def build_NN(self, lr):
 		self.initializer = tf.global_variables_initializer()
 		self.local_initializer = tf.local_variables_initializer()
-		self.create_placeholders()
-		self.preds = self.foward_pass(self.placeholder_x)
-		self.avg_cost = self.loss_op(self.preds, self.placeholder_y)
-		tf.summary.scalar('Cost', self.avg_cost)
-		self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.avg_cost)	
-		round_preds = tf.argmax(self.preds, 1)
-		round_lables = tf.argmax(self.placeholder_y, 1)
-		self.precision, _ = tf.metrics.precision(round_lables, round_preds)
-		self.recall, _ = tf.metrics.recall(round_lables, round_preds)
-		self.summary_op = tf.summary.merge_all()
+		with (self.graph).as_default():
+			self.create_placeholders()
 
+			self.preds = self.foward_pass(self.placeholder_x)
+			self.avg_cost = self.loss_op(self.preds, self.placeholder_y)
+			self.optimizer = tf.train.AdamOptimizer(lr).minimize(self.avg_cost)	
+
+			tf.summary.scalar('Cost', self.avg_cost)
+			self.summary_op = tf.summary.merge_all()
+
+
+			self.true_preds = tf.argmax(self.preds, 1)
+			self.true_labels = tf.argmax(self.placeholder_y, 1)
+			self.correct = tf.equal(self.true_preds, self.true_labels)
+			self.accuracy = tf.reduce_mean(tf.cast(self.correct, "float"))
+			self.precision, _ = tf.metrics.precision(self.true_labels, self.true_preds)
+			self.recall, _ = tf.metrics.recall(self.true_labels, self.true_preds)
 
 	def load_data(self):
 		self.train_x, self.train_y, self.eval_x, self.eval_y, self.test_x, self.test_y = read_data(self.data_filepath)
 
-	def train_NN(self, epochs, lr, show_graph=True):
-		self.epochs = epochs
-		self.lr = lr
-		nn.build_NN()
-		self.costs = []
-		self.iter_counter = 0
-		batch_costs = []
-		BATCH_SIZE = 16
-		num_batches = int(self.train_x.shape[0] / BATCH_SIZE) + 1
-		with tf.Session() as sess:
+	def train_NN(self, epochs, lr, batch_size=16, show_graph=True, print_costs=False):
+		iter_counter = 0 #counts iters for tensorboard
+		batch_costs = [] #costs across batches
+		costs = [] #avg batch costs
+		num_batches = int(self.train_x.shape[0] / batch_size) + 1
+		self.graph = tf.Graph()
+		nn.build_NN(lr)
+		with tf.Session(graph=self.graph) as sess:
 			# sess.run(self.initializer)
 			sess.run(tf.local_variables_initializer())
 			# tf.global_variables_initializer().run()
@@ -128,111 +105,116 @@ class NN(object):
 			summary_filename = "/train-" + str(int(time.time())) + ""
 			self.train_writer = tf.summary.FileWriter(self.logs_path + summary_filename, sess.graph) #creates a summary path for files 
 			
-			for i in range(1, self.epochs + 1):
-				# train_batch = list(zip(self.train_x, self.train_y))
-				# shuffle(train_batch)
-				# self.train_x, self.train_y = zip(*train_batch)				
+			for i in range(epochs):
 				item_order = list(range(self.train_x.shape[0]))
-				# shuffle(item_order)
-				# for b in range(len(self.train_x)):
+				shuffle(item_order)
 				for b in range(num_batches):
-					if (b == num_batches - 1) and (b*BATCH_SIZE != self.train_x.shape[0]):
-						item_indexes = item_order[b*BATCH_SIZE:]
+					if (b == num_batches - 1) and (b*batch_size != self.train_x.shape[0]):
+						item_indexes = item_order[b*batch_size:]
 					else:
-						item_indexes = item_order[b*BATCH_SIZE:(b+1)*BATCH_SIZE]
+						item_indexes = item_order[b*batch_size:(b+1)*batch_size]
 
 					cur_batch_x = self.train_x[item_indexes]
 					cur_batch_y = self.train_y[item_indexes]
 
-
 					cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
 					_, cur_cost, preds, summary = sess.run([self.optimizer, self.avg_cost, self.preds, self.summary_op], feed_dict=cur_feed)
-					# _, cur_cost, preds = sess.run([self.optimizer, self.avg_cost, self.preds], feed_dict=cur_feed)
 				
 					#tensorboard log			
-					self.train_writer.add_summary(summary, self.iter_counter) 
+					self.train_writer.add_summary(summary, iter_counter) 
 					self.train_writer.flush()
-					self.iter_counter += 1
+					iter_counter += 1
 
-					if (i % 10 == 0) and (b == 1):
-						print("Cost @", i, cur_cost)
+					if ((i + 1) % 10 == 0) and (b == 1) and print_costs:
+						print("Cost @", i + 1, cur_cost)
+
 					batch_costs += [cur_cost]
 
-				self.costs += [sum(batch_costs)/len(batch_costs)]
+				costs += [sum(batch_costs)/len(batch_costs)]
 			print(sess.run(self.precision, cur_feed))
-			if show_graph:
-				y = self.costs
-				x = [i for i in range(len(y))]
 
-				plt.plot(x, y)
-				plt.ylabel("Cost")
-				plt.xlabel("Epoch")
-				plt.show()
+			if show_graph:
+				plot_costs(costs)
 
 			score = self.eval_NN()
 			train_acc = score["avg_train_acc"]
 			eval_acc = score["avg_eval_acc"]
-			run_log = {"costs": self.costs, "train_acc": train_acc, "eval_acc": eval_acc, "lr" : self.lr, "epochs" : self.epochs}
+			run_log = {"costs": costs, "train_acc": train_acc, "eval_acc": eval_acc, "lr" : lr, "epochs" : epochs}
 			self.runs += [run_log]
+			
 			return sess
+
+	def plot_costs(costs):
+		epochs = list(range(len(costs)))
+		plt.plot(x, y)
+		plt.ylabel("Cost")
+		plt.xlabel("Epoch")
+		plt.show()
+
 
 	def predict(self, x):
 		preds = tf.argmax(se)
 
+	def eval_given_examples(self, examples_x, examples_y, batch_size=16):
+		batches_x, batches_y = create_batches(self.train_x, self.train_y, batch_size)
+		acc = []
+		precision = []
+		recall = []
+		# with tf.Session(graph=self.graph) as sess: 
+		# 	for b in range(len(batches_x)):
+		# 		cur_batch_x = batches_x[b]
+		# 		cur_batch_y = batches_y[b]
+		# 		print("SHAPE OF KFNDSFKL", np.shape(cur_batch_x))
+		# 		cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
+		# 		cur_acc, cur_precision, cur_recall = sess.run([self.accuracy, self.precision, self.recall])
+		# 		cur_precision = self.precision.eval(cur_feed)
+		# 		cur_recall = self.recall.eval(cur_feed)
+		# 		cur_acc = self.accuracy.eval(cur_feed)
+
+		# 		acc += [cur_acc]
+		# 		precision += [cur_precision]
+		# 		recall += [cur_recall]
+
+		# avg_acc = sum(acc)/len(acc)
+		# avg_precision = sum(precision)/len(precision)
+		# avg_recall = sum(recall)/len(recall)
+		for b in range(len(batches_x)):
+			cur_batch_x = batches_x[b]
+			cur_batch_y = batches_y[b]
+			cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
+			cur_precision = self.precision.eval(cur_feed)
+			cur_recall = self.recall.eval(cur_feed)
+			cur_acc = self.accuracy.eval(cur_feed)
+
+			acc += [cur_acc]
+			precision += [cur_precision]
+			recall += [cur_recall]
+
+		avg_acc = sum(acc)/len(acc)
+		avg_precision = sum(precision)/len(precision)
+		avg_recall = sum(recall)/len(recall)
+		return avg_acc, avg_precision, avg_recall
+
+
 	def eval_NN(self):
-		preds = tf.argmax(self.preds, 1)
-		labels = tf.argmax(self.placeholder_y, 1)
-		correct = tf.equal(preds, labels)
-		accuracy = tf.reduce_mean(tf.cast(correct, "float"))
-		train_acc = []
-		train_precision = []
-		train_recall = []
+		train_acc, train_precision, train_recall = self.eval_given_examples(self.train_x, self.train_y)
+		print("train_acc", train_acc)
+		print("train_precision", train_precision)
+		print("train_recall", train_recall)
 
-		batches_x, batches_y = create_batches(self.train_x, self.train_y, 16)
-		for b in range(len(batches_x)):
-			cur_batch_x = batches_x[b]
-			cur_batch_y = batches_y[b]
-			cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
-			cur_precision = self.precision.eval(cur_feed)
-			cur_recall = self.recall.eval(cur_feed)
-			cur_train_acc = accuracy.eval(cur_feed)
+		eval_acc, eval_precision, eval_recall = self.eval_given_examples(self.eval_x, self.eval_y)
+		print("eval_acc", eval_acc)
+		print("eval_precision", eval_precision)
+		print("eval_recall", eval_recall)
 
-			train_acc += [cur_train_acc]
-			train_precision += [cur_precision]
-			train_recall += [cur_recall]
+		# print("Average train accuracy:", avg_train_acc*100 , "%")
+		# print("Train precision / recall", train_precision, "/", train_recall)
+		# print("train f1 score", 2 * ((train_precision * train_recall) / (train_precision + train_recall)))
+		# print("Average eval accuracy:", avg_eval_acc*100 , "%")
+		# print("Train precision / recall", avg_eval_precision, "/", avg_eval_recall)
+		# print("train f1 score", 2 * ((avg_eval_precision * avg_eval_recall) / (avg_eval_precision + avg_eval_recall)))
 
-		avg_train_acc = sum(train_acc)/len(train_acc)
-		train_precision = sum(train_precision)/len(train_precision)
-		train_recall = sum(train_recall)/len(train_recall)
-
-		eval_acc = []
-		eval_precision = []
-		eval_recall = []
-		batches_x, batches_y = create_batches(self.eval_x, self.eval_y, 16)
-		for b in range(len(batches_x)):
-			cur_batch_x = batches_x[b]
-			cur_batch_y = batches_y[b]
-			cur_feed = {self.placeholder_x : cur_batch_x, self.placeholder_y : cur_batch_y}
-			cur_eval_acc = accuracy.eval(cur_feed)
-			cur_precision = self.precision.eval(cur_feed)
-			cur_recall = self.recall.eval(cur_feed)
-
-			eval_acc += [cur_eval_acc]
-			eval_precision += [cur_precision]
-			eval_recall += [cur_recall]
-
-		avg_eval_acc = sum(eval_acc)/len(eval_acc)
-		avg_eval_precision = sum(eval_precision)/len(eval_precision)
-		avg_eval_recall = sum(eval_recall)/len(eval_recall)
-
-		print("Average train accuracy:", avg_train_acc*100 , "%")
-		print("Train precision / recall", train_precision, "/", train_recall)
-		print("train f1 score", 2 * ((train_precision * train_recall) / (train_precision + train_recall)))
-		print("Average eval accuracy:", avg_eval_acc*100 , "%")
-		print("Train precision / recall", avg_eval_precision, "/", avg_eval_recall)
-		print("train f1 score", 2 * ((avg_eval_precision * avg_eval_recall) / (avg_eval_precision + avg_eval_recall)))
-
-		return {"avg_train_acc" : avg_train_acc, "avg_eval_acc" : avg_eval_acc}
+		return {"avg_train_acc" : train_acc, "avg_eval_acc" : eval_acc}
 
 	def compare_runs(self, show_graph=True):
 		for run in self.runs:
@@ -262,7 +244,7 @@ class NN(object):
 
 nn = NN()
 nn.load_data()
-nn.train_NN(10, .001, show_graph=False)
+nn.train_NN(10, .001, show_graph=False, print_costs=True)
 # nn.train_NN(10, .00025, show_graph=False)
 # print("Finished 2")
 # nn.train_NN(100, .01, show_graph=False)
